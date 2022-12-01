3d01a1d76cc61a2e9eb2e38299c54130e66f9a5e
nipunamunasinghe <nipunamunasinghe1029@gmail.com>
2022-12-01 19:18:14 +0530

    origin changed

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(
     cors({
-      origin: ["http://localhost:3000", "https://pinvent-app.vercel.app"],
+      origin: ["http://localhost:3000", "https://livecaresystem.vercel.app"],
       credentials: true,
     }));
 
