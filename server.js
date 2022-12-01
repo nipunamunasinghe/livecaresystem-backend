f912e72adda75e6da5272b9954a6f31fba5cec7b
Nipuna Munasinghe <106421197+nipunamunasinghe@users.noreply.github.com>
2022-12-01 22:37:21 +0530

    Origin url changed

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(
     cors({
-      origin: ["http://localhost:3000", "https://livecaresystem.vercel.app"],
+      origin: ["http://localhost:3000", "https://livecaresystem-frontend.vercel.app"],
       credentials: true,
     }));
 
