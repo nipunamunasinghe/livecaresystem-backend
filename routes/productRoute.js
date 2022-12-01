8661b5a6472b114059d6c93a46a8e0ccf6c419c3
nipunamunasinghe <nipunamunasinghe1029@gmail.com>
2022-12-01 18:54:17 +0530

    Initial Commit

---
+++ routes/productRoute.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const protect = require("../middleWare/authMiddleware");
+const {
+    createProduct,
+    getProducts,
+    getProduct,
+    deleteProduct,
+    updateProduct,
+   
+  } = require("../controllers/productController");
+  const { upload } = require("../utils/fileUpload");
+const router = express.Router();
+
+
+router.post("/", protect, upload.single("image"), createProduct);
+router.patch("/:id", protect, upload.single("image"), updateProduct);
+router.get("/", protect, getProducts);
+router.get("/:id", protect, getProduct);
+router.delete("/:id", protect, deleteProduct);
+
+module.exports = router;
